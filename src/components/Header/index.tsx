849ec448c8d9ddab40a35a3e4f7154abaa963489
diegoocezar <diego.bernardino@amedigital.com>
2021-07-05 23:06:17 -0300

    create global styles and Header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import logoImg from '../../assets/logo.svg';
+
+import {Container, Content} from './styles';
+
+export default function index() {
+  return (
+    <Container>
+      <Content>
+        <img src={logoImg} alt="dt money" />
+        <button type="button">Nova transação</button>
+      </Content>
+    </Container>
+  )
+}
