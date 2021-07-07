b192ff8f76ed4ab40860badcae3d8069d3260467
diegoocezar <diego.bernardino@amedigital.com>
2021-07-07 00:48:30 -0300

    use openModal function

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,12 +3,18 @@ import logoImg from '../../assets/logo.svg';
 
 import {Container, Content} from './styles';
 
-export default function index() {
+interface HeaderProps {
+  onOpenNewTransactionModal: () => void;
+}
+
+export default function Header({onOpenNewTransactionModal}: HeaderProps) {
+ 
   return (
     <Container>
       <Content>
         <img src={logoImg} alt="dt money" />
-        <button type="button">Nova transação</button>
+        <button onClick={onOpenNewTransactionModal} type="button">Nova transação</button>
+        
       </Content>
     </Container>
   )
